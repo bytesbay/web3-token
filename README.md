1a00e9df7e56698f19d3863f5e56596d47f35d1d
PersonalAccessToken <>
2021-09-26 18:59:15 +0300

    save

--- README.md
+++ README.md
@@ -14,12 +14,30 @@ $ npm i web3-token
 ```js
 import Web3Token from 'web3-token';
 
+// Connection to MetaMask wallet
+const web3 = new Web3(ethereum);
+await ethereum.enable();
 
+// getting address from which we will sign message
+const address = (await web3.eth.getAccounts())[0];
+
+// getting a token
+const token = await Web3Token.sign(msg => web3.eth.personal.sign(msg, address), '1d');
+
+// attaching token to authorization header ... for example
 ```
 
 ## Example usage (Server side)
 ```js
 import Web3Token from 'web3-token';
+
+// getting token from authorization header ... for example
+
+const { address, body } = await Web3Token.verify(token);
+
+// now you can find that user by his address
+
+req.user = await User.findOne({ address });
 ```
 
 ## License
--- src/main.js
+++ src/main.js
@@ -2,18 +2,21 @@ import Web3Token from './lib';
 
 const test = async () => {
 
+  // Connection to MetaMask wallet
   const web3 = new Web3(ethereum);
   await ethereum.enable();
 
-  const address = (await web3.eth.getAccounts())[0];
+  // getting address from which we will sign message
+  const your_address = (await web3.eth.getAccounts())[0];
 
-  const token = await Web3Token.sign(msg => web3.eth.personal.sign(msg, address), '1d');
+  // getting a token
+  const token = await Web3Token.sign(msg => web3.eth.personal.sign(msg, your_address), '1d');
 
   console.log('TOKEN CREATED', token);
 
-  const { address: rec_address, body } = await Web3Token.verify(token);
+  const { address, body } = await Web3Token.verify(token);
 
-  console.log('ADDRESS RECOVERED', rec_address, body);
+  console.log('ADDRESS RECOVERED', address, body);
 }
 
 document.querySelector('#btn').addEventListener('click', () => test())
